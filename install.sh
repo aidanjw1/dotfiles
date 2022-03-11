c0bf747db347b2931d594698e047104d34a893b6
Aidan White <aidan.white36@gmail.com>
2022-03-10 23:17:11 -0600

    install script updates

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
-# Link .config files to home directory
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/.dotfile/.tmux.conf ~/.tmux.conf
+if [[ "$SKIP_LINKS" != "true" ]]
+then
+    # Link .config files to home directory
+    ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+    ln -sf ~/.dotfiles/.zsh/* ~/.zsh
+    touch ~/.zsh/variables
+    ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+    ln -sf ~/.dotfiles/.gitconfig ~/.gitconfig
+    ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+fi
 
-# Install homebrew and Brewfile apps
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew bundle install
+if [[ "$SKIP_BREW" != "true" ]]
+then
+    # Install homebrew and Brewfile apps
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    brew bundle install
+fi
 
